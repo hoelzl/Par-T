a9b158e216c036c973fc135a8df8fcc1ac7be89f
Matthias Hoelzl <tc@xantira.com>
2012-10-27 16:14:05 +0200

    Remove quasiquote from Lisp, fix bootstrap

--- bootstrap-compiler.lisp
+++ bootstrap-compiler.lisp
@@ -587,41 +587,6 @@
 
 ;;; ==============================
 
-(defun quasi-q (x)
-  "Expand a quasiquote form into append, list, and cons calls."
-  (cond
-    ((vectorp x)
-     (list 'apply 'vector (quasi-q (coerce x 'list))))
-    ((atom x)
-     (if (constantp x) x (list 'quote x)))
-    ((starts-with x 'unquote)      
-     (assert (and (rest x) (null (rest2 x))))
-     (second x))
-    ((starts-with x 'quasiquote)
-     (assert (and (rest x) (null (rest2 x))))
-     (quasi-q (quasi-q (second x))))
-    ((starts-with (first x) 'unquote-splicing)
-     (if (null (rest x))
-         (second (first x))
-         (list 'append (second (first x)) (quasi-q (rest x)))))
-    (t (combine-quasiquote (quasi-q (car x))
-                           (quasi-q (cdr x))
-                           x))))
-
-(defun combine-quasiquote (left right x)
-  "Combine left and right (car and cdr), possibly re-using x."
-  (cond ((and (constantp left) (constantp right))
-         (if (and (eql (eval left) (first x))
-                  (eql (eval right) (rest x)))
-             (list 'quote x)
-             (list 'quote (cons (eval left) (eval right)))))
-        ((null right) (list 'list left))
-        ((starts-with right 'list)
-         (list* 'list left (rest right)))
-        (t (list 'cons left right))))
-
-;;; ==============================
-
 (defvar *trace-par-t-reader* nil)
 
 (defun load-par-t-file (file-name)
