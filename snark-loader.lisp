7e86fd17230dd4357b757923697d0e9eb00424aa
Matthias Hoelzl <tc@xantira.com>
2012-10-31 01:52:50 +0100

    Remove unnecessary files

--- snark-loader.lisp
+++
@@ -1,9 +0,0 @@
-;;; -*- Mode: Lisp; common-lisp-style: poem -*-
-
-;;; Copyright (c) 2012 Matthias Hölzl
-;;; 
-;;; This file is licensed under the MIT license; see the file LICENSE
-;;; in the root directory for further information.
-
-(in-package :parallel-thetis)
-
